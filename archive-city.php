c276f9c7c24150423479e2817bc31a503c3f3542
yannbug <yann@abc.fr>
2016-11-15 22:05:12 +0100

    liste régions

--- archive-city.php
+++ archive-city.php
@@ -6,6 +6,13 @@
 
 			<h1><?php _e( 'Cartes', 'html5blank' ); ?></h1>
 
+			<ul class="liste_regions vertical">
+				<?php $regions = get_terms( 'subdivision', array( 'parent'=>0 ) ); ?>
+				<?php foreach( $regions as $region ) : $region_svg = ucfirst( sanitize_title( preg_replace( '/\'/', '-', $region->name ) ) ); ?>
+					<li class="region"><a class="region_link" href="<?php echo get_term_link( $region ); ?>" region="<?php echo $region_svg; ?>"><?php echo strtoupper( $region->name ); ?></a></li>
+				<?php endforeach; ?>
+			</ul>
+			
 			<img id="france-svg" class="svg france regions" src="<?php echo get_template_directory_uri(); ?>/img/france-regions.svg" alt="France - régions" />
 			
 		</section>
